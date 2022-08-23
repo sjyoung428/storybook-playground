dec8ca947921e89bca3df280a7b1005fb9c76d3d
codeyoung <77968955+sjyoung123@users.noreply.github.com>
2022-08-23 17:54:31 +0900

    doc: Modal컴포넌트 JSDoc

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -3,6 +3,14 @@ import ReactDOM from "react-dom";
 import { useEffect } from "react";
 import { ModalProps } from "./Modal.types";
 
+/**
+ *
+ * @param {boolean} open - true일 때 모달이 열리고 false이면 모달이 닫힌다.
+ * @param {Pick<ModalProps,"setOpen">} setOpen - open의 상태를 변경하는 setState 배경을 클릭하면 모달창이 꺼진다.
+ * @param {ReactNode} children - 모달창 안에 나타나는 요소
+ *
+ */
+
 const Modal = ({ open = false, setOpen, children }: ModalProps) => {
   const modalRoot = document.querySelector("#modal") as HTMLElement;
 
