f5fd253390a5c8cba20b92b44d2251b4755393d6
codeyoung <77968955+sjyoung123@users.noreply.github.com>
2022-08-27 22:23:22 +0900

    refactor: svg변환

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,3 +1,4 @@
+import SVGArrowRight from "../Svg/SVGArrowRight";
 import * as S from "./Button.styles";
 import { ButtonProps } from "./Button.types";
 
@@ -18,20 +19,7 @@ const Button = ({
     <S.Container {...rest} size={size} color={color}>
       <S.ButtonValue>{children}</S.ButtonValue>
       <S.ButtonArrow color={color}>
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          strokeWidth={1.5}
-          stroke="currentColor"
-          className="w-6 h-6"
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            d="M13.5 4.5L21 12m0 0l-7.5 7.5M21 12H3"
-          />
-        </svg>
+        <SVGArrowRight />
       </S.ButtonArrow>
     </S.Container>
   );
