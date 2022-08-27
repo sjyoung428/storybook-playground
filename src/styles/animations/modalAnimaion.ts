6db9656e22dbb1f1c810139d3356b635192a97d9
codeyoung <77968955+sjyoung123@users.noreply.github.com>
2022-08-27 20:01:14 +0900

    수정

--- src/styles/animations/modalAnimaion.ts
+++ src/styles/animations/modalAnimaion.ts
@@ -4,7 +4,6 @@ import { fadeOut } from "../keyframes/fadeOut";
 
 export const modalAnimation = (open: boolean) => css`
   visibility: ${open ? "visible" : "hidden"};
-  z-index: 15;
   animation: ${open ? fadeIn : fadeOut} 0.3s ease-out;
   transition: visibility 0.3s ease-out;
 `;
